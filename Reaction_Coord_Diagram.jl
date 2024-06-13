3b9d875f7a4d65ee0aa85719fecd83c65af87a88
bragulaET <115180217+bragulaET@users.noreply.github.com>
2024-06-13 11:55:35 -0400

    Update Reaction_Coord_Diagram.jl

--- Reaction_Coord_Diagram.jl
+++ Reaction_Coord_Diagram.jl
@@ -2,8 +2,6 @@ using Plots
 
 energies_1 = [0.0 24.3 -48.7]
 
-# energies_2 = [-41.9 17.8 0.0 15.3 5.1 43.5 -30.4]
-
 function construct_plot_segments(energies)
 
     plotted_energies = []
@@ -72,41 +70,6 @@ function make_plot(energies)
 
 end
 
-function make_plot2(energies)
-
-    plot_segments = construct_plot_segments(energies)
-    total_segments = length(plot_segments)
-    plot_list = []
-
-    counter = 1.0
-
-    for i in 1:total_segments
-        if mod(i,2) == 1
-
-            if i == 1
-                p1 = plot!(counter:counter+4,plot_segments[i],color = :green,legend = false,linestyle = :solid,
-                          annotations = (counter+2,plot_segments[i][1]+1.1,"$(plot_segments[i][1])"),annotationfontsize = 6)
-                counter += 4.0
-                push!(plot_list,p1)
-            else
-                p1 = plot!(counter:counter+4,plot_segments[i],color = :green,legend = false,linestyle = :solid,
-                           annotations = (counter+2,plot_segments[i][1]+1.1,"$(plot_segments[i][1])"),annotationfontsize = 6)
-                counter += 4.0
-                push!(plot_list,p1)
-            end
-
-        else
-            p1 = plot!(counter:counter+8,plot_segments[i],linestyle = :dash,legend = false,color = :green)
-            counter += 8.0
-            push!(plot_list,p1)
-        end
-    end
-
-    return plot_list,counter
-
-end
-
-# plots_list2, counter2 = make_plot2(energies_2)
 plots_list1, counter1 = make_plot(energies_1)
 
 p1 = plot(plots_list1[1], grid = false, xaxis = true, yguide = "E \n kcal/mol", xguide = "Reaction Coordinate",
@@ -114,15 +77,4 @@ p1 = plot(plots_list1[1], grid = false, xaxis = true, yguide = "E \n kcal/mol", 
           xticks = false,
           annotation = (0.0,maximum(energies_1)+1,text('\u27A4',7,:black,rotation=90)))
 annotate!(counter1,minimum(energies_1)-1,text('\u27A4',7,:black,rotation=0))
-
-# p2 = plot!(plots_list2[1], grid = false, xaxis = false, yguide = "E \n kcal/mol",
-#           xlims = [0.0, counter2], ylims = [min(minimum(energies_2),minimum(energies_1))-1,max(maximum(energies_2),maximum(energies_1))+1],
-#           annotation = (0.0,max(maximum(energies_2),maximum(energies_1))+1,text('\u27A4',7,:black,rotation=90)))
-
-
-
-#If you want two plots to show up together there's some roundabout hack you need.
-#First, run the script only generating p1. That is, run this code with "plot(p1)" below.
-#Then, rerun this again with both plot(p1) and plot(p2) together.
-#Not entirely sure why that is occuring but for now at least this works.
 
